ec26903bf7ccc25ddac920bc6f3622e81bf48748
dariussh77 <dariussh77@gmail.com>
2023-05-17 20:30:21 +0300

    papildytas straipsniaiContext reduceriu ir getu

--- src/contexts/StraipsniaiContext.jsx
+++ src/contexts/StraipsniaiContext.jsx
@@ -1,11 +1,32 @@
-import { createContext } from "react";
+import { createContext,useEffect,useReducer } from "react";
 const StraipsniaiContext=createContext();
-
+const StraipsniaiAction={
+    get:"get_straipsniai",
+    add:"add_straipsniai"
+};
+const reducer=(state,action)=>{
+    switch(action.type){
+        case StraipsniaiAction.get: return action.data;
+        default: return state;
+    }
+};
 const StraipsniaiProvider = ({children}) => {
-    
+    const [straipsniai,setStraipsniai]=useReducer(reducer,[]);
+    useEffect(()=>{
+        fetch(`http://localhost:7777/straipsniai`)
+            .then(res=>res.json())
+            .then(data=>setStraipsniai({
+                type:StraipsniaiAction.get,
+                data:data
+            }))
+    },[]);
     return ( 
         <StraipsniaiContext.Provider
-            value={{}}
+            value={{
+                straipsniai,
+                setStraipsniai,
+                StraipsniaiAction
+            }}
         >
             {children}
         </StraipsniaiContext.Provider>
