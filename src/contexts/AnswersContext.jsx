e914f326589201e056566d08f205f1814c10de4a
dariussh77 <dariussh77@gmail.com>
2023-05-19 10:03:43 +0300

    sukurtas AnswersContext

---
+++ src/contexts/AnswersContext.jsx
@@ -0,0 +1,34 @@
+import { createContext, useEffect, useReducer } from "react";
+const AnswerAction={
+    get:"get_answers",
+    edit:"edit_answer"
+};
+const AnswersContext= createContext();
+const reducer=(state,action)=>{
+    switch(action.type){
+        case AnswerAction.get:return action.data;
+        default: return state;
+    }
+}
+const AnswersProvider = ({children}) => {
+    const [answers,setAnswers]=useReducer(reducer,[]);
+    useEffect(()=>{
+        fetch(`http://localhost:7777/answers`)
+        .then(res=>res.json())
+        .then(data=>setAnswers({
+            type:AnswerAction.get,
+            data:data
+        }))
+    },[]);
+    return ( 
+        <AnswersContext.Provider
+            value={{
+
+            }}
+        >
+            {children}
+        </AnswersContext.Provider>
+     );
+}
+export {AnswersProvider };
+export default AnswersContext;
--- src/index.js
+++ src/index.js
@@ -7,18 +7,21 @@ import { StraipsniaiProvider } from './contexts/StraipsniaiContext';
 import { TemosProvider } from './contexts/TemosContext';
 import { QuestionsProvider } from './contexts/QuestionsContext';
 import { UsersProvider } from './contexts/UsersContext';
+import { AnswersProvider } from './contexts/AnswersContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <UsersProvider>
-        <QuestionsProvider>
-            <TemosProvider>
-                <StraipsniaiProvider>
-                    <BrowserRouter>
-                        <App />
-                    </BrowserRouter>
-                </StraipsniaiProvider>
-            </TemosProvider>
-        </QuestionsProvider>
-    </UsersProvider>
+    <AnswersProvider>
+        <UsersProvider>
+            <QuestionsProvider>
+                <TemosProvider>
+                    <StraipsniaiProvider>
+                        <BrowserRouter>
+                            <App />
+                        </BrowserRouter>
+                    </StraipsniaiProvider>
+                </TemosProvider>
+            </QuestionsProvider>
+        </UsersProvider>
+    </AnswersProvider>
 );
