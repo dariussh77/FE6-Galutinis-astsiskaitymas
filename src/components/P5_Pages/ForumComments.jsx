8318c64676a0ae2a6c95ed595fcfe2a5b6323e4a
dariussh77 <dariussh77@gmail.com>
2023-05-19 08:31:44 +0300

    ForumComment puslapyje papildytas Question divas

--- src/components/P5_Pages/ForumComments.jsx
+++ src/components/P5_Pages/ForumComments.jsx
@@ -1,19 +1,43 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState,useContext } from "react";
+import QuestionsContext from "../../contexts/QuestionsContext";
+import UsersContext from "../../contexts/UsersContext";
 import { useParams } from "react-router-dom";
+import styled from 'styled-components';
+const MainCommentsCSS=styled.main`
+    border: 1px solid black;
+    width: 100%;
+`;
 const ForumComments = () => {
+    const{questions}=useContext(QuestionsContext);
+    const{users}=useContext(UsersContext);
+    console.log('questions: ', questions);
     const {id}=useParams();
     const [currentQ, setcurrentQ]=useState();
-    
     useEffect(()=>{
         fetch(`http://localhost:7777/questions/${id}`)
             .then(res=>res.json())
             .then(data=>setcurrentQ(data));
     },[]);
     return ( 
-        <main>
-            { currentQ&&<h2>{currentQ.question}</h2> }
-                      
-        </main>
+        <MainCommentsCSS>
+            <div className="questionDiv">
+                { 
+                    currentQ&&
+                    <>
+                        <h1>KLAUSIMAS:</h1>
+                        <h2>{currentQ.question}</h2> 
+                        <img src={users.find(e=>e.id===currentQ.creator).avatar} alt={users.find(e=>e.id===currentQ.creator).userName} />
+                        <h4>{users.find(e=>e.id===currentQ.creator).userName}</h4>
+                        <h5>{currentQ.tema}</h5>
+                    </>
+                }        
+            </div>
+            <div className="commentsDiv">
+                
+            </div>
+
+                
+        </MainCommentsCSS>
      );
 }
  
