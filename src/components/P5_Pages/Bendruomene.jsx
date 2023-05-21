32221284373b71332bf8c5d19db376aa43a35cf8
dariussh77 <dariussh77@gmail.com>
2023-05-21 16:00:26 +0300

    patvarkyta Userių puslapis

--- src/components/P5_Pages/Bendruomene.jsx
+++ src/components/P5_Pages/Bendruomene.jsx
@@ -1,11 +1,30 @@
 import { useContext } from "react";
 import UsersContext from "../../contexts/UsersContext";
 import UserCard from "../P2_Molecules/UserCard";
+import { styled } from "styled-components";
+const MainBCss=styled.main`
+    >div{
+        .usercard{
+            border: 1px solid black;
+            display: flex;
+            flex-direction: column;
+            gap:10px;
+            img{
+                width:100px;
+                border: 1px solid black;
+                border-radius: 50px;
+            }
+        }
+        display: flex;
+        flex-wrap: wrap;
+        gap:20px
+    }
+`;
 
 const Bendruomene = () => {
     const {users,currentUser}=useContext(UsersContext);
     return ( 
-        <main>
+        <MainBCss>
             <h1>Mūsų nariai:</h1>
             <div>
                 {
@@ -16,7 +35,7 @@ const Bendruomene = () => {
                     />)
                 }
             </div>
-        </main>
+        </MainBCss>
      );
 }
  
